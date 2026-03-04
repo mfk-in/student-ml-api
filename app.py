a012bd3bde590015715a59cf6e21f65f44ada891
mfk-in <mahnamfatimakhan@gmail.com>
2026-03-05 00:44:57 +0500

    Added dynamic port for deployment

--- app.py
+++ app.py
@@ -25,5 +25,5 @@ def predict():
         "predicted_score" : round(prediction[0], 2)
     })
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",5000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
